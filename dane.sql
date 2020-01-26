b0169c786b99c97b520c348bf76e7061084cf2c8
TurboReal <58658995+TurboReal@users.noreply.github.com>
2020-01-26 23:42:16 +0100

    update

--- dane.sql
+++ dane.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 26 Sty 2020, 08:19
+-- Czas generowania: 26 Sty 2020, 23:41
 -- Wersja serwera: 10.3.18-MariaDB-50+deb10u1.cba
 -- Wersja PHP: 7.1.33
 
@@ -37,17 +37,25 @@ CREATE TABLE `dane` (
   `mail` varchar(100) COLLATE latin1_general_ci NOT NULL,
   `address` varchar(100) COLLATE latin1_general_ci NOT NULL,
   `login` varchar(100) COLLATE latin1_general_ci NOT NULL,
-  `password` varchar(100) COLLATE latin1_general_ci NOT NULL
+  `password` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  `userType` int(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
 -- Zrzut danych tabeli `dane`
 --
 
-INSERT INTO `dane` (`name`, `secondName`, `lastName`, `dateOfBirth`, `citizenship`, `pesel`, `phoneNr`, `mail`, `address`, `login`, `password`) VALUES
-('Wojtek', 'Marcin', 'Krab', '2018-09-03', 'Poland', '12345678909', '123456789', 'mail@mail.com', 'Ulica', 'login', 'haslo'),
-('Zbigniew', 'Tomasz', 'Szbki', '2014-09-03', 'Poland', '12345671234', '987654321', 'mail2@mail.com', 'Ulicaa', 'haslo', 'login'),
-('Artur', 'Marcin', 'Barcelona', '2004-12-03', 'Niemcy', '56725678909', '1745556789', 'mail3@mail.com', 'Ulica', 'login', 'haslo');
+INSERT INTO `dane` (`name`, `secondName`, `lastName`, `dateOfBirth`, `citizenship`, `pesel`, `phoneNr`, `mail`, `address`, `login`, `password`, `userType`) VALUES
+('Wojtek', 'Marcin', 'Krab', '2018-09-03', 'Poland', '12345678909', '123456789', 'mail@mail.com', 'Ulica', 'login', 'haslo', 0),
+('Zbigniew', 'Tomasz', 'Szbki', '2014-09-03', 'Poland', '12345671234', '987654321', 'mail2@mail.com', 'Ulicaa', 'haslo', 'login', 0),
+('Artur', 'Marcin', 'Barcelona', '2004-12-03', 'Niemcy', '56725678909', '1745556789', 'mail3@mail.com', 'Ulica', 'login', 'haslo', 0),
+('Imie', 'Imie2', 'Nazwisko', '2018-09-03', 'Poland', '98765405164', '666999123', 'mail@mail.com', 'Ulica', 'login3', 'haslo3', 1),
+('Imie', 'Imie2', 'Nazwisko', '2018-09-03', 'Poland', '98765405164', '666999123', 'mail@mail.com', 'Ulica', 'login5', 'haslo5', 1),
+('Imie', 'Imie2', 'Nazwisko', '2018-09-03', 'Poland', '98765405164', '666999123', 'mail@mail.com', 'Ulica', 'login4', 'haslo4', 0),
+('Imie', 'Imie2', 'Nazwisko', '2018-09-03', 'Poland', '98765405164', '666999123', 'mail@mail.com', 'Ulica', 'login6', 'haslo7', 0),
+('Imie', 'Imie2', 'Nazwisko', '2018-09-03', 'Poland', '98765405164', '666999123', 'mail@mail.com', 'Ulica', 'login8', 'haslo8', 0),
+('Imie', 'Imie2', 'Nazwisko', '2018-09-03', 'Poland', '98765405164', '666999123', 'mail@mail.com', 'Ulica', 'login9', 'haslo9', 2),
+('Imie', 'Imie2', 'Nazwisko', '2018-09-03', 'Poland', '98765405164', '666999123', 'mail@mail.com', 'Ulica', 'login10', 'haslo10', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
